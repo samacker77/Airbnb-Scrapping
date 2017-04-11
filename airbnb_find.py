023a1b3239ba0acc48c3d9eacb076b0eb5f1ff3a
Samar Srivastava <samarsrivastava44@gmail.com>
2017-04-12 01:56:49 +0530

    A simple Python script to scrap Airbnb data for a given location, dates and number of gates.
    A simple Python script to scrap Airbnb data for a given location, dates and number of gates.

---
+++ airbnb_find.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
+location = input("Enter your location: ")
+check_in = input("Enter Check-in date: ")
+check_out = input("Enter Check-out date: ")
+guests = input("Enter number of guests: ")
+url = "https://www.airbnb.co.in/s/"+location+"/homes?adults=2&checkin="+check_in+"&checkout="+check_out+"&guests="+guests+"&place_id=ChIJwe1EZjDG5zsRaYxkjY_tpF0&allow_override%5B%5D=&s_tag=N9fRr9A1"
+airbnb_r = requests.get(url)
+airbnb_soup = BeautifulSoup(airbnb_r.text, 'html.parser')
+for link in airbnb_soup.findAll('a'):
+	uprint(link)
